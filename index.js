b2ae7eb7d24188f44466b15a9971dd8e1556435b
Obuli Sai Naren <66naren@gmail.com>
2021-10-19 19:05:02 +0530

    Implemented User Login & Signup
    Both Front and Back End.

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var fs = require('fs');
+const bcrypt = require("bcrypt");
+
+const port = process.env.PORT || 6006;      // Set Port
+const app = express();                      // Use Express
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(express.text());
+app.use(express.urlencoded({extended: true}));
+
+
+// Render Homepage
+app.get('/', (request, response) => {
+    console.log('Hello Home!');
+    response.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/login', function (req, res) {
+    var user = req.body['user'];
+    var pass = req.body['password'];
+    console.log(req.body);
+    fs.readFile('public/js/users.json', 'utf8', async function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var obj = JSON.parse(data);
+            if (user in obj) {
+                console.log('User Found', pass, obj[user].password);
+                const validPassword = await bcrypt.compare(pass, obj[user].password);
+                if (validPassword) {
+                    obj[user]['currUser'] = true;
+                    var json = JSON.stringify(obj); //convert it back to json
+                    fs.writeFile('public/js/users.json', json, 'utf8', function writeFileCallback() {
+                    });
+                    console.log({message: "Valid password"});
+                    res.send(true);
+                } else {
+                    console.log({error: "Invalid Password"});
+                    res.send(false);
+                }
+            } else {
+                res.send(false);
+            }
+        }
+    });
+});
+
+app.post('/signup', async function (req, res) {
+    var user = req.body['user'];
+    var pass = req.body['password'];
+    const salt = await bcrypt.genSalt(10);
+    var password = await bcrypt.hash(pass, salt);
+    console.log(req.body, password);
+
+    fs.readFile('public/js/users.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var obj = JSON.parse(data);
+            obj[user] = {user: user, password: password};
+            var json = JSON.stringify(obj);
+            fs.writeFile('public/js/users.json', json, 'utf8', function writeFileCallback() {
+            });
+        }
+    });
+    res.send(true);
+});
+
+// Serve Static Files
+app.use(express.static('public'));
+
+// Listen to Port
+app.listen(port, () => {
+    console.log('Node Server running at : ', port)
+    console.log(__dirname);
+    console.log(('http://localhost:' + port))
+});
---
+++ public/js/script.js
@@ -0,0 +1,171 @@
+$(".log-in").click(function () {
+    $(".signIn").addClass("active-dx");
+    $(".signUp").addClass("inactive-sx");
+    $(".signUp").removeClass("active-sx");
+    $(".signIn").removeClass("inactive-dx");
+});
+
+$(".back").click(function () {
+    $(".signUp").addClass("active-sx");
+    $(".signIn").addClass("inactive-dx");
+    $(".signIn").removeClass("active-dx");
+    $(".signUp").removeClass("inactive-sx");
+});
+
+salert = document.getElementById('successalert');
+stext = document.getElementById('succ_span');
+sname = document.getElementById('alert_uname');
+simg = document.getElementById('userimg');
+
+document.getElementById('successcloser').addEventListener('click', function (e) {
+    $("#successalert").fadeOut(3000);
+});
+
+
+function pg_trans() {
+    var layerClass = "." + 'top' + "-layer";
+    var layers = document.querySelectorAll(layerClass);
+    for (const layer of layers) {
+        layer.classList.toggle("active");
+    }
+}
+
+var outer_container = document.getElementById("outer-container");
+
+function proceed_user(user_name, olnw) {
+    if (olnw === 'login') {
+        stext.innerHTML = 'Welcome Back!';
+        sname.innerHTML = user_name;
+    } else if (olnw === 'signup') {
+        $('#userimg').attr("src", "img/kiss.gif");
+        stext.innerHTML = 'Hey New One!';
+        sname.innerHTML = user_name;
+    }
+    salert.style.display = 'block';
+    setTimeout(function () {
+        $("#successalert").fadeOut(1000);
+        $('head').append('<link href="css/bground.css" media="all" rel="stylesheet">');
+        outer_container.style.background = "linear-gradient(45deg, #a770ef, #cf8bf3, #fdb99b)";
+        outer_container.style.backgroundSize = "400% 400%";
+        outer_container.style.animation = "gradient 2s infinite ease-in-out;"
+    }, 2000);
+    $("#login-image").fadeOut(1000);
+    $("#color-bg").fadeIn(1000);
+    pg_trans();
+}
+
+ualert = document.getElementById('useralert');
+document.getElementById('alertcloser').addEventListener('click', function (e) {
+    $("#useralert").fadeOut(3000);
+});
+
+
+function stop_user() {
+    ualert.style.display = 'block';
+    setTimeout(function () {
+        $("#useralert").fadeOut(3000);
+    }, 5000);
+}
+
+$("#login").submit(function (e) {
+    var log_name = document.getElementById('log-name').value;
+    var log_pass = document.getElementById('log-pass').value;
+    e.preventDefault();
+    $.ajax({
+        url: 'http://localhost:6006/login',
+        type: 'POST',
+        data: {
+            user: log_name,
+            password: log_pass
+        },
+        success: function (msg) {
+            console.log('User logged in', msg);
+            if (msg) {
+                proceed_user(log_name, 'login');
+            } else {
+                stop_user();
+            }
+        }
+    });
+    return false;
+});
+
+var my_json;
+$.getJSON("js/users.json", function (data) {
+    my_json = data;
+});
+
+
+$("#signup").submit(function (e) {
+    var sign_name = document.getElementById('sign-name').value;
+    var sign_pass = document.getElementById('sign-pass').value;
+    var sign_repass = document.getElementById('sign-repass').value;
+    console.log(document.getElementById('sign-name').value);
+    e.preventDefault();
+    if (sign_pass === sign_repass) {
+        console.log(sign_name, sign_pass, sign_repass);
+        $.ajax({
+            url: 'http://localhost:6006/signup',
+            type: 'POST',
+            data: {
+                user: sign_name,
+                password: sign_pass
+            },
+            success: function (msg) {
+                console.log('User Created', msg);
+                if (msg) {
+                    proceed_user(sign_name, 'signup');
+                } else {
+                    stop_user();
+                }
+            }
+        });
+    }
+    return false;
+});
+
+var signup_name = document.getElementById('sign-name')
+var signup_pass = document.getElementById('sign-pass');
+var signup_repass = document.getElementById('sign-repass');
+
+signup_name.addEventListener('keyup', function (e) {
+    $('#availusr').css("display", "initial");
+    var tname = signup_name.value;
+    if (tname in my_json) {
+        console.log(tname, "Not Available");
+        $('#availusr').attr("src", "img/red.png");
+    } else {
+        console.log("Available");
+        $('#availusr').attr("src", "img/green.png");
+    }
+});
+
+signup_pass.addEventListener('keyup', function (e) {
+    $('#availpass').css("display", "initial");
+    var tpass = signup_pass.value;
+    if (tpass.length >= 4) {
+        $('#availpass').attr("src", "img/green.png");
+    } else {
+        $('#availpass').attr("src", "img/red.png");
+    }
+});
+
+signup_repass.addEventListener('keyup', function (e) {
+    $('#availpass').css("display", "initial");
+    $('#availrepass').css("display", "initial");
+    var tpass = signup_pass.value;
+    var trepass = signup_repass.value;
+    console.log(tpass, trepass);
+    if (tpass === trepass) {
+        $('#availrepass').attr("src", "img/green.png");
+    } else {
+        $('#availrepass').attr("src", "img/red.png");
+    }
+});
+
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+    setTimeout(function () {
+        document.getElementById("log-name").focus();
+    }, 3500);
+});
